250b3cfcdf091d922bfc4066843703b2250730a1
Ibrahima Sane <iss654864@gmail.com>
2024-12-24 13:31:59 +0000

    Récupération

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,9 +89,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'rh_db'),
+        'NAME': os.getenv('DB_NAME', 'rhh'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'passer'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'azerty12'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
